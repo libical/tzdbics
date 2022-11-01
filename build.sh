34ef836acdecfc23e145c58bc09fb2de759fb7ef
Markus Minichmayr <markus@tapkey.com>
2022-11-01 10:39:19 +0100

    Simplify build.sh and settings.config by removing unused functionality. No need to download the master from externally.

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@
 # This script does
 #
 # * download the specified tzdata archive
-# * download the specified master zoneinfo archive
 # * build vzic
 # * run vzic on the downloaded tzdata
 # * merge the new zoneinfo into the master zoneinfo
@@ -45,40 +44,17 @@ if [ -d build/zoneinfo ]; then rm -r build/zoneinfo; fi
 mkdir -p build/zoneinfo
 ./build/vzic/vzic --olson-dir tzdata --output-dir build/zoneinfo --pure
 
-if [[ ${VZIC_MASTER_ZONEINFO_ARCHIVE_URL} ]]; then
-    echo "Downloading master zoneinfo from $VZIC_MASTER_ZONEINFO_ARCHIVE_URL"
-    wget -q -O build/zoneinfo_master.tar.gz $VZIC_MASTER_ZONEINFO_ARCHIVE_URL
-
-    export VZIC_MASTER_ZONEINFO_ARCHIVE_PATH=build/zoneinfo_master.tar.gz
-fi
-
 export VZIC_ZONEINFO_NEW=`pwd`/build/zoneinfo
 
-if [[ ${VZIC_MASTER_ZONEINFO_ARCHIVE_PATH} ]]; then
-    echo "Extracting master zoneinfo $VZIC_MASTER_ZONEINFO_ARCHIVE_PATH"
-    mkdir -p build/zoneinfo_master
-    tar -xzf $VZIC_MASTER_ZONEINFO_ARCHIVE_PATH -C build/zoneinfo_master
-
-    # define variables used in vzic-merge.pl
-    export VZIC_ZONEINFO_MASTER=`pwd`/build/zoneinfo_master
-elif [[ -d zoneinfo ]]; then
-    echo "Using existing zoneinfo as master."
+if [[ -d zoneinfo ]]; then
     export VZIC_ZONEINFO_MASTER=`pwd`/zoneinfo
-fi
 
-if [[ ${VZIC_ZONEINFO_MASTER} ]]; then
-
-    echo "Merging"
+    echo "Merging..."
     ./vzic-merge.pl
 
     # copy updated zones
     cp -r $VZIC_ZONEINFO_NEW/zones.* $VZIC_ZONEINFO_MASTER 
 
-    if [ "$VZIC_ZONEINFO_MASTER" != "`pwd`/zoneinfo" ]; then 
-        if [ -d zoneinfo ]; then rm -r zoneinfo; fi
-        mkdir -p zoneinfo
-        cp -r $VZIC_ZONEINFO_MASTER zoneinfo
-    fi
 else
     cp -r "$VZIC_ZONEINFO_NEW" zoneinfo
     export VZIC_ZONEINFO_MASTER=`pwd`/zoneinfo
